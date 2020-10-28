5f6614141de974deb0ccbe47cb7a250c7b6e967d
amirhossein <torkanlooa@gmail.com>
2020-10-28 20:30:24 +0330

    major abstract added

---
+++ src/utils/Cell.java
@@ -0,0 +1,5 @@
+package utils;
+
+public class Cell {
+    // TODO: 10/28/20 fill this cell class with objects of neighbour cells
+}
---
+++ src/utils/Map.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Map {
+    // TODO: 10/28/20 this class should have a 2d array representing game's table. each one is an object from Cell class.
+
+}
